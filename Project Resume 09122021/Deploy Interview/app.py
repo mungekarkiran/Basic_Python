fba6e2771537e1a6b1972861425bad2b860f87b8
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-01-09 19:10:55 +0530

    09012022 - 2
    09012022 - 2

---
+++ Project Resume 09122021/Deploy Interview/app.py
@@ -0,0 +1,13 @@
+# lib's
+from flask import Flask, render_template, request
+
+# start app
+app = Flask(__name__)
+
+# routing
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) #debug=True
