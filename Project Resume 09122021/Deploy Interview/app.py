b577236d28099e29773127a8085b8b4012280ecb
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-01-11 00:59:51 +0530

    10012022
    10012022

--- Project Resume 09122021/Deploy Interview/app.py
+++ Project Resume 09122021/Deploy Interview/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # routing
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.htm')
 
 if __name__ == '__main__':
     app.run(debug=True) #debug=True
