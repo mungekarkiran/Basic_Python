18d1d60f9da3c31847c41c36bc8d2df2b4ce4647
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-01-13 00:48:10 +0530

    13022022
    13022022

--- Project Resume 09122021/Deploy Interview/app.py
+++ Project Resume 09122021/Deploy Interview/app.py
@@ -13,6 +13,15 @@ def index():
 def qanda():
     return render_template('qanda.htm')
 
+@app.route('/jobtitle', methods=['GET', 'POST'])
+def jobtitle():
+    return render_template('jobtitle.htm')
+
+@app.route('/videointerview', methods=['GET', 'POST'])
+def videointerview():
+    return render_template('videointerview.htm')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True) #debug=True
