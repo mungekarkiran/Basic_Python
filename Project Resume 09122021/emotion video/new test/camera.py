0c5a5da6f042cf49157197f5f849ac6d97db5e2a
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-02-02 21:29:24 +0530

    02022022
    02022022

--- Project Resume 09122021/emotion video/new test/camera.py
+++ Project Resume 09122021/emotion video/new test/camera.py
@@ -1,12 +1,12 @@
 import cv2
 from imutils.video import WebcamVideoStream
 import numpy as np
-from keras.models import  load_model
+from keras.models import load_model
 from keras.preprocessing import image
 
 
 # load model
-model = load_model("best_model.h5")
+model = load_model("best_model1.h5")
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
@@ -28,28 +28,32 @@ class VideoCamera(object):
         # additional code
         cv2.putText(image1, 'Score : ', (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
-        gray_img = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+        # gray_img = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+        gray_img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
         faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
-        for (x, y, w, h) in faces_detected:
-            cv2.rectangle(image1, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
-            roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
-            roi_gray = cv2.resize(roi_gray, (224, 224))
-            img_pixels = image.img_to_array(roi_gray)
-            img_pixels = np.expand_dims(img_pixels, axis=0)
-            img_pixels /= 255
+        try: 
+            for (x, y, w, h) in faces_detected:
+                cv2.rectangle(image1, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+                roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from image
+                # roi_gray = cv2.resize(roi_gray, (224, 224))
+                roi_gray = cv2.resize(roi_gray, (48, 48))
+                img_pixels = image.img_to_array(roi_gray)
+                img_pixels = np.expand_dims(img_pixels, axis=0)
+                img_pixels /= 255
 
-            predictions = model.predict(img_pixels)
+                predictions = model.predict(img_pixels)
 
-            # find max indexed array
-            max_index = np.argmax(predictions[0])
+                # find max indexed array
+                max_index = np.argmax(predictions[0])
 
-            emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise')
-            predicted_emotion = emotions[max_index]
+                emotions = ('Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise')
+                predicted_emotion = emotions[max_index]
 
-            cv2.putText(image1, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
-
+                cv2.putText(image1, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        except Exception as e:
+            print('Exception : ', e)
+            pass
 
         _, jpeg = cv2.imencode('.jpg', image1)
         data = []
