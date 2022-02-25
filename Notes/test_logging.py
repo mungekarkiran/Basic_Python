b03ec568812a89d3af300db77499eaa2de7d1886
Kiran Krushnakant Mungekar <mungekarkiran05@gmail.com>
2022-02-25 19:05:28 +0530

    25022022 logging
    25022022 logging

---
+++ Notes/test_logging.py
@@ -0,0 +1,95 @@
+from email import message
+import logging
+from time import asctime
+
+# logging.basicConfig(filename='test.log')
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.shutdown()
+
+
+# logging.basicConfig(filename='test1.log', level = logging.INFO)
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.shutdown()
+
+
+
+# logging.basicConfig(filename='test2.log', level = logging.DEBUG, format='%(asctime)s %(message)s')
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.shutdown()
+
+
+
+
+# logging.basicConfig(filename='test3.log', level = logging.DEBUG, format='%(asctime)s %(message)s')
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.debug('My debug log!')
+
+# logging.shutdown()
+
+
+
+
+
+# logging.basicConfig(filename='test4.log', level = logging.DEBUG, format='%(levelname)s %(asctime)s %(message)s')
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.debug('My debug log!')
+
+# logging.shutdown()
+
+
+
+
+# logging.basicConfig(filename='test5.log', level = logging.ERROR, format='%(levelname)s %(asctime)s %(message)s')
+
+# logging.info('My info log!')
+
+# logging.warning('My warning log!')
+
+# logging.error('My error log!')
+
+# logging.debug('My debug log!')
+
+# logging.shutdown()
+
+
+
+
+logging.basicConfig(filename='test6.log', level = logging.INFO, format='%(levelname)s %(asctime)s %(message)s')
+
+logging.info('My info log!')
+
+logging.warning('My warning log!')
+
+logging.error('My error log!')
+
+logging.debug('My debug log!')
