ad15fa44bc138826a0441df49d6322923d18901e
Kiran Krushnakant Mungekar <mungekarkiran05@gmail.com>
2022-03-01 19:17:18 +0530

    01032022 - 1
    01032022 - 1

---
+++ Notes/class_module_notes/test1.py
@@ -0,0 +1,23 @@
+def addnum(a,b):
+    '''
+    To add 2 number.
+    '''
+    return a+b
+
+def mulnum(a,b):
+    '''
+    To multiplay 2 number.
+    '''
+    return a*b
+
+def divnum(a,b):
+    '''
+    To divide 2 number.
+    '''
+    return a/b
+
+def myfun():
+    '''
+    To print a statement.
+    '''
+    print('This is my function in module test1.')
---
+++ Notes/class_module_notes/test2.py
@@ -0,0 +1,18 @@
+data = {
+    'name':'kiran',
+    'l_name':'mungekar',
+    'age':27,
+    'addr':'virar'
+}
+
+def getname():
+    '''
+    To get the name. 
+    '''
+    return data['name']
+
+def getaddr():
+    '''
+    To get the address. 
+    '''
+    return data['addr']
