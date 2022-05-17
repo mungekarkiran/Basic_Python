0637436182d54c50d4c00c6321498e4917d64d8f
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-05-18 00:02:56 +0530

    18052022 - 1
    18052022 - 1

--- Mini Project Chronic_Kidney_Disease/Deployment/app.py
+++ Mini Project Chronic_Kidney_Disease/Deployment/app.py
@@ -8,14 +8,9 @@ from werkzeug.utils import secure_filename
 import pickle
 import sqlite3
 
-
 # variable's
 connection = sqlite3.connect('login.db', timeout=1, check_same_thread=False)
 cursor = connection.cursor()
-col_list = ['age', 'blood_pressure', 'specific_gravity', 'albumin', 'sugar', 'red_blood_cells', 'pus_cell',
-            'pus_cell_clumps', 'bacteria', 'blood_glucose_random', 'blood_urea', 'serum_creatinine', 'sodium',
-            'potassium', 'haemoglobin', 'packed_cell_volume', 'white_blood_cell_count', 'red_blood_cell_count',
-            'hypertension', 'diabetes_mellitus', 'coronary_artery_disease', 'appetite', 'peda_edema', 'aanemia']
 
 # create idpass table
 try:
@@ -32,7 +27,7 @@ app = Flask(__name__)
 # routing
 @app.route('/')
 def index():
-    return render_template('index.htm')
+    return render_template('index.html')
 
 @app.route('/mylogin', methods=['POST'])
 def mylogin():
@@ -40,7 +35,6 @@ def mylogin():
         email = request.form.get('email')
         password = request.form.get('password')
         level = request.form.get('level')
-
         # print(email, password, level, '\n')
 
         cursor.execute(f'''SELECT * FROM idpass WHERE email = "{email}" AND pass = "{password}" AND level = "{level}"; ''')
@@ -50,9 +44,9 @@ def mylogin():
         # print('result : ', result)
 
         if len(result) > 0 and level == 'doctor' or level == 'other':
-            return render_template('home.htm', col_names= col_list)
+            return render_template('home.html')
         # elif len(result) > 0 and level == 'other':
-        #     return render_template('home.htm')
+        #     return render_template('home.html')
         else:
             return '<b>Wrong email, password!</b>'
         
@@ -63,20 +57,15 @@ def myreg():
         email = request.form.get('email')
         password = request.form.get('password')
         level = request.form.get('level')
-        # print(email, password, level, '\n')
         
         try:
-            # connection = sqlite3.connect('login.db', timeout=10)
-            # cursor = connection.cursor()
-            # time.sleep(1)
-            
             cursor.execute(f'''INSERT INTO idpass (email, pass, level) VALUES ("{email}", "{password}", "{level}"); ''')
             connection.commit()
-            time.sleep(2)
-            return render_template('index.htm')
+            time.sleep(1)
+            return render_template('index.html')
         except Exception as e:
             print('Reg. Exception : ',e,'\n')
-            return render_template('index.htm')
+            return render_template('index.html')
 
 
 if __name__ == '__main__':
