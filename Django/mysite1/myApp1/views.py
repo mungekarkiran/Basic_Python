ab3f8006c242d8498308499b2124a07a437f9da0
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-05-31 00:38:07 +0530

    31052022 - Django
    31052022 - Django

--- Django/mysite1/myApp1/views.py
+++ Django/mysite1/myApp1/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def indexPage(request):
-    return HttpResponse('This is indexPage ....')
+    context = {
+        'variable' : 'hello django!!!'
+    }
+    # return HttpResponse('This is indexPage ....')
+    return render(request, 'index.html', context)
 
 def about(request):
     return HttpResponse('This is about ....')
