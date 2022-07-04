3c1c81295c617c238050ca308718bd02d808ec9c
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-07-05 02:29:32 +0530

    05072022 - POMO
    05072022 - POMO

--- Project POMO/data scraping/POMO_classifier.py
+++ Project POMO/data scraping/POMO_classifier.py
@@ -18,9 +18,11 @@ from sklearn.metrics import accuracy_score
 # from sklearn.metrics import precision_score
 # from sklearn.metrics import recall_score
 # from sklearn.metrics import f1_score
+from sklearn.metrics import zero_one_loss
 from sklearn.metrics import hamming_loss
 
-from sklearn.metrics import confusion_matrix
+
+from sklearn.metrics import multilabel_confusion_matrix # confusion_matrix
 
 import pickle
 import re
@@ -82,11 +84,14 @@ doc = docx.Document()
 doc.add_heading(f'Classification Report {CURRENT_TIME_STAMP}', 0)
 
 
-def builMultiLabelModels(X_train, y_train, classifierModel):
+def builMultiLabelModels(train, test, classifierModel):
     
     logging.info(f"Initializing builMultiLabelModels")
     doc.add_paragraph(f"Initializing builMultiLabelModels")
     
+    X_train, y_train = train
+    X_test, y_test = test
+
     multiLabelModel = MultiOutputClassifier(classifierModel, n_jobs=2)
     
     classifierModel = multiLabelModel.fit(X_train, y_train)
@@ -95,19 +100,38 @@ def builMultiLabelModels(X_train, y_train, classifierModel):
     
     Accuracy_score = round((accuracy_score(y_train, y_pred))*100,2)
     Loss = round((1-accuracy_score(y_train, y_pred))*100,2)
+    Zero_one_loss = round(zero_one_loss(y_train, y_pred)*100,2)
     Hamming_loss = round((hamming_loss(y_train, y_pred))*100,2)
-
-    # print("Accuracy_score:", Accuracy_score,'%')
-    # print("Loss:", Loss,'%')
-    # print("Hamming_loss:", Hamming_loss,'%')
-    # print("Classification_report:\n",metrics.classification_report(y_train, y_pred))
     
+    doc.add_paragraph(f"Classification on Training dataset : ")
     doc.add_paragraph(f"Accuracy_score: {Accuracy_score} %")
     doc.add_paragraph(f"Loss: {Loss} %")
+    doc.add_paragraph(f"Zero_one_loss: {Zero_one_loss} %")
     doc.add_paragraph(f"Hamming_loss: {Hamming_loss} %")
     doc.add_paragraph(f"Classification_report: {metrics.classification_report(y_train, y_pred)}")
 
-    logging.info(f"'Accuracy': {Accuracy_score}, 'Loss': {Loss}, 'Hamming_loss': {Hamming_loss}")
+    logging.info(f"Classification on Training dataset : [ 'Accuracy': {Accuracy_score}, 'Loss': {Loss}, 'Zero_one_loss': {Zero_one_loss}, 'Hamming_loss': {Hamming_loss} ]")
+
+
+    classifierModel = multiLabelModel.fit(X_test, y_test)
+    
+    y_pred = classifierModel.predict(X_test)
+    
+    Accuracy_score_ = round((accuracy_score(y_test, y_pred))*100,2)
+    Loss_ = round((1-accuracy_score(y_test, y_pred))*100,2)
+    Zero_one_loss_ = round(zero_one_loss(y_test, y_pred)*100,2)
+    Hamming_loss_ = round((hamming_loss(y_test, y_pred))*100,2)
+    
+    doc.add_paragraph(f"Classification on Testing dataset : ")
+    doc.add_paragraph(f"Accuracy_score: {Accuracy_score_} %")
+    doc.add_paragraph(f"Loss: {Loss_} %")
+    doc.add_paragraph(f"Zero_one_loss: {Zero_one_loss_} %")
+    doc.add_paragraph(f"Hamming_loss: {Hamming_loss_} %")
+    doc.add_paragraph(f"Classification_report: {metrics.classification_report(y_test, y_pred)}")
+
+    logging.info(f"Classification on Testing dataset : [ 'Accuracy': {Accuracy_score_}, 'Loss': {Loss_}, 'Zero_one_loss': {Zero_one_loss_}, 'Hamming_loss': {Hamming_loss_} ]")
+
+
     logging.info(f"Done builMultiLabelModels")
     return classifierModel, {'Accuracy': Accuracy_score, 'Loss': Loss, 'Hamming_loss': Hamming_loss}
 
@@ -140,11 +164,14 @@ def classification_report_ensemble_voting_classifier(y_train, y_pred_new):
 
     Accuracy_score = round((accuracy_score(y_train, y_pred_new))*100,2)
     Loss = round((1-accuracy_score(y_train, y_pred_new))*100,2)
+    Zero_one_loss = round(zero_one_loss(y_train, y_pred_new)*100,2)
     Hamming_loss = round((hamming_loss(y_train, y_pred_new))*100,2)
+    # ML_confusion_matrix = multilabel_confusion_matrix(y_train, y_pred_new)
+    ML_confusion_matrix = metrics.classification_report(y_train, y_pred_new)
+    return (Accuracy_score, Loss, Zero_one_loss, Hamming_loss, ML_confusion_matrix)
 
-    return (Accuracy_score, Loss, Hamming_loss)
 
-def fit_ensemble_voting_classifier(X_train, y_train, classifiers):
+def fit_ensemble_voting_classifier(train, test, classifiers):
     
     logging.info("========== Initializing fit_ensemble_voting_classifier ==========\n\n")
     doc.add_heading(f"========== Initializing fit_ensemble_voting_classifier ==========\n\n", 2)
@@ -152,11 +179,13 @@ def fit_ensemble_voting_classifier(X_train, y_train, classifiers):
 
     for classifier in classifiers:
 
+        # X_train, y_train, X_test, y_test  = train, test
+
         logging.info(f"Model : {type(classifier).__name__} running...")
         # print(f'Model : {type(classifier).__name__}')
         doc.add_heading(f"Model : {type(classifier).__name__}\n", 2)
 
-        model, data = builMultiLabelModels(X_train, y_train, classifier)
+        model, data = builMultiLabelModels(train, test, classifier)
 
         # save the model to disk
         FILE_NAME = f'MultiLabelModel_{type(classifier).__name__}'
@@ -183,20 +212,44 @@ def fit_ensemble_voting_classifier(X_train, y_train, classifiers):
     logging.info(f"Result file saved : {RESULT_FILE_PATH}")
 
     try:
-        y_pred_new = predict_ensemble_voting_classifier(X_train, RESULT_FILE_PATH)
+        y_pred_new = predict_ensemble_voting_classifier(train[0], RESULT_FILE_PATH)
 
-        Accuracy_score, Loss, Hamming_loss = classification_report_ensemble_voting_classifier(y_train, y_pred_new)
+        Accuracy_score, Loss, Zero_one_loss, Hamming_loss, ML_confusion_matrix = classification_report_ensemble_voting_classifier(train[1], y_pred_new)
         
         print("Classification Report on Training dataset : ")
         print("Accuracy_score:", Accuracy_score,'%')
         print("Loss:", Loss,'%')
+        print("Zero_one_loss:", Zero_one_loss,'%')
         print("Hamming_loss:", Hamming_loss,'%')
+        print("ML_confusion_matrix:\n", ML_confusion_matrix)
 
         doc.add_heading(f"Ensemble_Voting_Classifier_Result >>> \n", 2)
         doc.add_paragraph(f"Classification Report on Training dataset : ")
         doc.add_paragraph(f"Accuracy_score: {Accuracy_score} %")
         doc.add_paragraph(f"Loss: {Loss} %")
+        doc.add_paragraph(f"Zero_one_loss: {Zero_one_loss} %")
         doc.add_paragraph(f"Hamming_loss: {Hamming_loss} %")
+        doc.add_paragraph(f"ML_confusion_matrix:\n{ML_confusion_matrix}")
+
+
+        y_pred_new = predict_ensemble_voting_classifier(test[0], RESULT_FILE_PATH)
+
+        Accuracy_score, Loss, Zero_one_loss, Hamming_loss, ML_confusion_matrix = classification_report_ensemble_voting_classifier(test[1], y_pred_new)
+        
+        print("Classification Report on Testing dataset : ")
+        print("Accuracy_score:", Accuracy_score,'%')
+        print("Loss:", Loss,'%')
+        print("Zero_one_loss:", Zero_one_loss,'%')
+        print("Hamming_loss:", Hamming_loss,'%')
+        print("ML_confusion_matrix:\n", ML_confusion_matrix)
+
+        doc.add_paragraph(f"Classification Report on Testing dataset : ")
+        doc.add_paragraph(f"Accuracy_score: {Accuracy_score} %")
+        doc.add_paragraph(f"Loss: {Loss} %")
+        doc.add_paragraph(f"Zero_one_loss: {Zero_one_loss} %")
+        doc.add_paragraph(f"Hamming_loss: {Hamming_loss} %")
+        doc.add_paragraph(f"ML_confusion_matrix:\n{ML_confusion_matrix}")
+
 
     except Exception as e:
         logging.error(f"Exception : [ {e} ]")
