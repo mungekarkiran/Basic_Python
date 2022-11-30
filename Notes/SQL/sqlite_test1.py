01e5b8b36fe1e437c73e7ed36c7e6ece27caf3a9
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2022-11-30 15:21:21 +0530

    file with links

--- Notes/SQL/sqlite_test1.py
+++ Notes/SQL/sqlite_test1.py
@@ -1,5 +1,9 @@
 # https://datacarpentry.org/python-ecology-lesson/09-working-with-sql/index.html
 # https://www.digitalocean.com/community/tutorials/how-to-use-the-sqlite3-module-in-python-3
+# https://www.sqlitetutorial.net/sqlite-import-csv/
+# https://www.sqlservertutorial.net/sql-server-basics/sql-server-insert-multiple-rows/
+# https://www.digitalocean.com/community/tutorials/sql-insert-multiple-rows
+
 import sqlite3
 
 # Create a SQL connection to our SQLite database
@@ -7,7 +11,7 @@ connect = sqlite3.connect('test.db') # portal_mammals.sqlite
 cursor = connect.cursor()
 
 # Create table
-cursor.execute("""CREATE TABLE faculty (fid INT, fname TEXT, qualification TEXT, deptid INTEGER);""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS faculty (fid INT, fname TEXT, qualification TEXT, deptid INTEGER);""")
 
 # Insert data
 cursor.execute("""INSERT INTO faculty VALUES (1, 'Aman', 'B.Tech', 1);""")
@@ -19,3 +23,15 @@ cursor.execute("""INSERT INTO faculty VALUES (2, 'Mohan', 'M.Tech', 1),
                                              (5, 'Ravi', 'Ph.D', 3),
                                              (6, 'Aarti', 'M.Tech', 2);""")
 connect.commit()
+
+
+# Create table student and insert multiple records
+
+
+
+# Creating a Table from an Existing Table
+# https://www.tutorialspoint.com/sql/sql-create-table-using-tables.htm
+# https://www.techonthenet.com/sql/tables/create_table2.php
+# CREATE TABLE SALARY AS
+# SELECT ID, SALARY
+# FROM CUSTOMERS;
