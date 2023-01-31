8a1d8465a1ab0ca877f42d354c164873e6084f03
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2023-01-31 22:14:23 +0530

    20230131 added links

--- Send Email/main.py
+++ Send Email/main.py
@@ -1,11 +1,18 @@
+# https://www.youtube.com/watch?v=QKeywJ4WOfM
+# https://www.youtube.com/watch?v=JLLE11Eo4fc
+# https://www.youtube.com/watch?v=66ZtJI-06bQ
+# https://www.youtube.com/watch?v=DTOXB1ApzRo
+# https://www.youtube.com/watch?v=Cp37ORZhJcE
+# https://www.youtube.com/watch?v=g_j6ILT-X0k
+
 from email.message import EmailMessage
 import ssl
 import smtplib
 
-email_sender = "mungekarkiran05@gmail.com"
+email_sender = "m5@gmail.com"
 email_password = "byzvpuvtiiqztaqm" # app password for device
 
-email_receiver = "mungekarkiran05@gmail.com"
+email_receiver = "m5@gmail.com"
 
 subject = "Dont forget to add subject"
 body = """
