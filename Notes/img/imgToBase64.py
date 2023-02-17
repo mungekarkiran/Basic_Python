09a9d2353850fd5a793a353280db8211871af33f
mungekarkiran <73243176+mungekarkiran@users.noreply.github.com>
2023-02-17 16:23:19 +0530

    20230217 added audio and video to base64

--- Notes/img/imgToBase64.py
+++ Notes/img/imgToBase64.py
@@ -9,6 +9,30 @@ print(f"base64_image : {base64_image}")
 # base32_image = base64.b32encode(data).decode('utf-8')
 # print(f'Base32-encoded image: {base32_image}')
 
+# Audio to base64
+# get sample sample audio file
+# https://voiceage.com/Audio-Samples-AMR-WB.html
+
+# Read the audio file
+with open('audio_file.wav', 'rb') as f:
+    audio_bytes = f.read()
+
+# Convert audio to base64
+audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
+# Print the base64-encoded audio
+print(audio_base64)
+
+
+# Read the audio file
+with open('video_file.mp4', 'rb') as f:
+    video_bytes = f.read()
+
+# Convert video to base64
+video_base64 = base64.b64encode(video_bytes).decode('utf-8')
+# Print the base64-encoded video
+print(video_base64)
+
+
 
 html = f'''
 <!DOCTYPE html>
@@ -18,6 +42,16 @@ html = f'''
 </head>
 <body>
 	<img id="image" src="data:image/jpeg;base64,{base64_image}">
+    <br><br>
+    <audio controls>
+        <source src="data:audio/wav;base64,{audio_base64}" type="audio/wav">
+        Your browser does not support the audio element.
+    </audio>
+    <br><br>
+    <video controls>
+        <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
+    </video>
+
 </body>
 </html>
 '''
