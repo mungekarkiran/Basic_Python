59eec81a1a31c1548b475bb13113a1d7a2b70d7e
Kiran Krushnakant Mungekar <73243176+mungekarkiran@users.noreply.github.com>
2024-10-31 01:40:34 +0530

    forcast page update
    forcast page update

--- Neha Project/Project Solar Care/Deployment/Pages/Forecast_Page.py
+++ Neha Project/Project Solar Care/Deployment/Pages/Forecast_Page.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 # Load pre-trained models
 def load_model(model_path):
@@ -13,9 +14,12 @@ temperature_model_path = 'models/temp_model.pkl'
 dew_model_path = 'models/dew_model.pkl'
 humidity_model_path = 'models/humidity_model.pkl'
 
-temperature_model = load_model(temperature_model_path)
-dew_model = load_model(dew_model_path)
-humidity_model = load_model(humidity_model_path)
+try:
+    temperature_model = load_model(temperature_model_path)
+    dew_model = load_model(dew_model_path)
+    humidity_model = load_model(humidity_model_path)
+except Exception as e:
+    st.warning(f"Warning : 404 : Time series models not found.", icon=None)
 
 # Forecasting function
 def forecast_next_24_hours(start_date):
@@ -32,9 +36,9 @@ def forecast_next_24_hours(start_date):
     # Create a DataFrame with results
     forecast_data = pd.DataFrame({
         'DateTime': forecast_dates,
-        'Temperature': round(temperature_forecast,2),
-        'Dew': round(dew_forecast,2),
-        'Humidity': round(humidity_forecast,2)
+        'temp': round(temperature_forecast,2), # Temperature
+        'dew': round(dew_forecast,2), # Dew
+        'humidity': round(humidity_forecast,2) # Humidity
     })
     
     return forecast_data
@@ -54,5 +58,38 @@ def forecast_page():
         st.write("Forecast for the next 24 hours:")
         st.write(forecast_df.set_index("DateTime"))
 
-        # # Optionally plot the forecast
+        # Optionally plot the forecast
         st.line_chart(forecast_df.set_index("DateTime"))
+
+        data = forecast_df.drop(["DateTime"], axis=1)
+
+        classification_model_path = 'models/classification_model.pkl'
+        try:
+            classification_model = load_model(classification_model_path)
+        except Exception as e:
+            st.warning(f"Warning : 404 : Classification models not found.", icon=None)
+
+        # Make prediction
+        prediction = classification_model.predict(data)
+        forecast_df['pre_label'] = prediction
+        
+        # Display the prediction result
+        st.write("Prediction result: ")
+        st.write(forecast_df.set_index("DateTime"))
+        st.line_chart(forecast_df.set_index("DateTime"))
+
+        # Pie Chart in Column 1        
+        st.write("### Pie Chart of `Need to Clean` Distribution")
+        pre_label_counts = forecast_df['pre_label'].value_counts()
+        fig_pie, ax_pie = plt.subplots()
+        ax_pie.pie(pre_label_counts, labels=pre_label_counts.index, autopct='%1.1f%%', startangle=90)
+        ax_pie.axis('equal')  # Equal aspect ratio ensures that the pie chart is a circle.
+        st.pyplot(fig_pie)
+        
+        # Final prediction
+        mgs= ''
+        if round((forecast_df['pre_label'].sum()/24)*100, 1) > 50 :
+            msg = '### Your Solar Panel need to Clean.'
+        else :
+            msg = '### No need to Clean your Solar Panel.'
+        st.write(msg)
