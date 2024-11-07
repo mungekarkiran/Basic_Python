410c24061ba6d4b4069d8bc05664dbe07e2012c2
Kiran Krushnakant Mungekar <73243176+mungekarkiran@users.noreply.github.com>
2024-11-08 00:03:59 +0530

    app update
    app update

--- Neha Project/Project Solar Care/Deployment/app.py
+++ Neha Project/Project Solar Care/Deployment/app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from Pages.Train_Model_Page import home_page
 from Pages.Forecast_Page import forecast_page
+from Pages.Classification_Page import classification_page
 
 # Sidebar for navigation
 st.sidebar.title("Solar Care")
-page = st.sidebar.selectbox("Go to", ["Home", "Forecast"])
+page = st.sidebar.selectbox("Go to", ["Home - Train Your Model", "Forecast for Next 24 Hours", "Real-Time Maintenance Classification"])
 
 # Call the selected page's function
-if page == "Home":
+if page == "Home - Train Your Model":
     home_page()
-elif page == "Forecast":
+elif page == "Forecast for Next 24 Hours":
     forecast_page()
+elif page == "Real-Time Maintenance Classification":
+    classification_page()
